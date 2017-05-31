7bfd6095545de12683fb2fa27457928138e6fa97
Pisopen <stephane.piso@gmail.com>
2017-05-31 09:18:50 +0200

    update commit

--- src/Livre.java
+++ src/Livre.java
@@ -1,7 +1,7 @@
 /**
  * Created by Stephane on 24/05/2017.
  */
-public class Livre extends Document implements Interface{
+public abstract class Livre extends Document implements Interface{
     public Livre(int num_renseignement, String titre) {
         super(num_renseignement, titre);
     }
--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,14 @@ public class Main {
         my_bibli.add_etage(etagere);
         Document roman = new Roman(13, "Sol", "Saul", 150, "EMI");
         Document revue = new Revue(12, "Dol", 05, 2016);
-        Document livre = new Livre(12, "Pol", "Paul", 150);
-        etagere.add_doc(livre); //ajout livre
+        Document manuel = new Manuel(12, "Pol", "Paul", 150,"CM2");
+        etagere.add_doc(manuel); //ajout manuel
         etagere.add_doc(roman); //ajout roman
         etagere.add_doc(revue); //ajout revue
         etagere.Imprimer(); //test impression
         etagere.triEtagere(); //tri etagere
-        System.out.println(etagere.search_auteur("Saul")); //recherche auteur
-        System.out.println(etagere.search_titre("Dol")); //recherche titre
+        System.out.println(etagere);
+        //System.out.println(etagere.search_auteur("Saul")); //recherche auteur
+        //System.out.println(etagere.search_titre("Dol")); //recherche titre
     }
 }
